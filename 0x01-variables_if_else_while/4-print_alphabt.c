140b00279740311c3234b3cfd20eb24bb82249f3
OmarYoussef <omarelgoerany97@gmail.com>
2023-03-21 14:10:10 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Descroiption
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z' && ch != 'q' && ch != 'e')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
