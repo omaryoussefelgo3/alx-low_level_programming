2a4a98b3f455b53d7ad2375857121c6f3f3e693b
OmarYoussef <omarelgoerany97@gmail.com>
2023-03-21 14:41:24 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * digits
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + 48);
+	}
+	putchar('\n');
+}
