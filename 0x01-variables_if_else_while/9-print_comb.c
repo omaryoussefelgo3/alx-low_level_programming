df2efb4502c3d6e630bb40b6d98dc7dbe9aa81ea
OmarYoussef <omarelgoerany97@gmail.com>
2023-03-21 15:27:20 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Count
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++);
+
+	{
+		putchar(i + 48);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
