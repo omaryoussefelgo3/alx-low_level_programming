7d13b6b6c53617750a2cf21d3a64dafc4fd9b313
OmarYoussef <omarelgoerany97@gmail.com>
2023-03-21 14:26:21 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * counting numbers
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%i", i);
+	}
+	return (0);
+}
