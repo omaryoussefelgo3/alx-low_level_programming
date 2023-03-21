038217f5490ef0e45d9ef1de1487f977f6debb40
OmarYoussef <omarelgoerany97@gmail.com>
2023-03-21 14:58:48 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Rev
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i <= 0; i++)
+	{
+		putchar(i + 48);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
