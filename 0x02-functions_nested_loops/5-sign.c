5d364954efa23281b970e315423a3e428f763681
OmarYoussef <omarelgoerany97@gmail.com>
2023-03-22 09:33:25 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar(+);
 		return (1);
 	}
 	else if (n == 0)
@@ -20,7 +20,7 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar(45);
 		return (-1);
