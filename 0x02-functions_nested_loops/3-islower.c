d3587228d3636678d73bd1a782494eaa352d8f54
OmarYoussef <omarelgoerany97@gmail.com>
2023-03-22 08:46:29 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,14 +13,7 @@
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-	{
 		_putchar(1);
 		return (0);
-	}
-	else if (c <= 97 && c >= 122)
-	{
-		_putchar(0);
-		return (0);
-	}
 	_putchar('\n');
 }
