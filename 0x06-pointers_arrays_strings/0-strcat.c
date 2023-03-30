8360cff49c2a6f4ad5b6a816ce10f46696101239
OmarYoussef <omarelgoerany97@gmail.com>
2023-03-30 07:39:29 -0700

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _strcat - function
+ * @dest: char
+ * @src: char
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	for (j = 0; src[j]; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
