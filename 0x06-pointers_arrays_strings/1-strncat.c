fcc23bb1f70b404f1a5097fd109b8087592df32c
OmarYoussef <omarelgoerany97@gmail.com>
2023-03-30 08:52:36 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - function
+ * @dest: char
+ * @src : char
+ * @n: int
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len, i;
+
+	len = 0;
+	while (dest[len])
+		len++;
+	for (i = 0; i < n && src[i] != '\n'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\n';
+	return (dest);
+}
