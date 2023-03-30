652f7b2fbc98a0734305eeae669410ef70251727
OmarYoussef <omarelgoerany97@gmail.com>
2023-03-30 12:27:23 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+char *string_toupper(char *n);
+char *string_toupper(char *);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
