3b143856f9378522f3d0a2dd86fd7c23b57b84bf
OmarYoussef <omarelgoerany97@gmail.com>
2023-04-03 19:44:47 -0700

    4-strpbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H 
 #define MAIN_H
 #include <stdio.h>
+char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(char c);
