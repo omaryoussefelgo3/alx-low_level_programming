cd12e9c8e51a279d48273fa926fd3ab7cfdd8169
OmarYoussef <omarelgoerany97@gmail.com>
2023-04-04 09:09:52 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsums - func
+ * @a: int
+ * @size: int
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - itr - 1];
+		a += size;
+	}
+	printf("%d, " s1);
+	printf("%d\n", s2);
+}
